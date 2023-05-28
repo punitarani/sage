716923ff1addfb5332fec1bd66c1ea8418fe813d
Punit Arani <punitsai36@gmail.com>
2023-05-27 17:06:25 -0700

    Fix download and add invalid doi error

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import asyncio
 
 import streamlit as st
 
-from sage.unpaywall import get_paper_info
+from sage.unpaywall import get_paper_info, download_paper
 
 if __name__ == "__main__":
     st.set_page_config(page_title="sage", page_icon="🎓", layout="wide")
@@ -19,9 +19,19 @@ if __name__ == "__main__":
     with st.spinner("Fetching paper info..."):
         paper_info = asyncio.run(get_paper_info(doi_input))
 
+    if not paper_info:
+        st.error("Invalid DOI")
+        st.stop()
+
     st.header(paper_info["title"])
     st.markdown(f"[{paper_info['doi']}]({paper_info['doi_url']})")
-    st.download_button("Download PDF", paper_info["best_oa_location"]["url_for_pdf"])
 
     with st.expander("Full info"):
         st.json(paper_info)
+
+    with st.spinner("Downloading paper..."):
+        download_path = asyncio.run(download_paper(doi_input))
+
+    with open(download_path, "rb") as f:
+        download_data = f.read()
+    st.download_button("Download PDF", data=download_data, file_name=f"{doi_input}.pdf")
