eb635e9da8730793bbad2ac46d2a4665b94222a6
Punit Arani <punitsai36@gmail.com>
2023-05-28 01:13:11 -0700

    Remove load_dotenv() assert

--- sage/__init__.py
+++ sage/__init__.py
@@ -8,4 +8,4 @@ PROJECT_DIR = Path(__file__).parent.parent
 if os.environ.get("env", "dev") != "prod":
     from dotenv import load_dotenv
 
-    assert load_dotenv(PROJECT_DIR.joinpath(".env"))
+    load_dotenv(PROJECT_DIR.joinpath(".env"))
