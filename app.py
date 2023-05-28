2bb65c248c9a041b7a55fcd7e5db6355a181f3b3
Punit Arani <punitsai36@gmail.com>
2023-05-28 06:11:33 -0700

    Add checkboxes to select similar papers

--- app.py
+++ app.py
@@ -8,10 +8,8 @@ import asyncio
 
 import streamlit as st
 
-from sage.document import load_pdf_document
 from sage.openalex import find_similar_papers, get_openalex_work
-from sage.summarize import summarize_text_abstractive
-from sage.unpaywall import get_paper_info, download_paper
+from sage.unpaywall import get_paper_info
 
 paper_openalex_works = {}
 paper_infos = {}
@@ -28,12 +26,8 @@ async def get_paper_data(paper):
     if sim_doi:
         sim_paper_info = await get_paper_info(doi=sim_doi)
         paper_infos[sim_entity_id] = sim_paper_info
-
-        sim_paper_title = sim_paper_info.get("title", None)
-        if not sim_paper_title:
-            sim_paper_title = sim_paper_info.get("doi", None)
-
-        return f"{sim_paper_title} ({sim_score})"
+        return sim_paper_info
+    return None
 
 
 async def main():
@@ -60,25 +54,26 @@ async def main():
 
     with st.spinner("Loading similar papers..."):
         coroutines = [get_paper_data(paper) for paper in similar_papers]
-        similar_paper_titles = await asyncio.gather(*coroutines)
+        similar_paper_infos = await asyncio.gather(*coroutines)
 
-    with st.expander("Similar papers"):
-        for title in similar_paper_titles:
-            st.write(title)
+    sim_paper_checkboxes = {}
 
-    with st.spinner("Downloading paper..."):
-        download_path = await download_paper(doi_input)
+    st.header("Similar papers")
+    for sim_paper_info in similar_paper_infos:
+        # Get title of similar paper
+        sim_paper_title = sim_paper_info.get("title", None)
+        if not sim_paper_title:
+            sim_paper_title = sim_paper_info.get("doi", None)
 
-    with open(download_path, "rb") as f:
-        download_data = f.read()
-    st.download_button("Download PDF", data=download_data, file_name=f"{doi_input}.pdf")
+        # Display checkbox for each similar paper
+        checkbox = st.checkbox(sim_paper_title)
+        sim_paper_checkboxes[sim_paper_title] = checkbox
 
-    with st.spinner("Summarizing paper..."):
-        text = load_pdf_document(download_path)
-        summary = await summarize_text_abstractive(text)
-
-    st.header("Summary")
-    st.write(summary)
+    if st.button("Generate"):
+        st.header("Selected papers")
+        for sim_paper_title, checkbox in sim_paper_checkboxes.items():
+            if checkbox:
+                st.write(sim_paper_title)
 
 
 if __name__ == "__main__":
