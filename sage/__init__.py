0e88300a19b79726f580242b475c00e5b95710bf
Punit Arani <punitsai36@gmail.com>
2023-05-28 01:10:08 -0700

    Dont load .env in prod

--- sage/__init__.py
+++ sage/__init__.py
@@ -1,9 +1,11 @@
 """sage package"""
 
+import os
 from pathlib import Path
-
-from dotenv import load_dotenv
 
 PROJECT_DIR = Path(__file__).parent.parent
 
-assert load_dotenv(PROJECT_DIR.joinpath(".env"))
+if os.environ.get("env", "dev") != "prod":
+    from dotenv import load_dotenv
+
+    assert load_dotenv(PROJECT_DIR.joinpath(".env"))
