bb7ba8295843cf61f844b8c0ded8b9aefdc19a47
Punit Arani <punitsai36@gmail.com>
2023-05-27 22:17:47 -0700

    Add find_similar_papers()

---
+++ sage/openalex.py
@@ -0,0 +1,94 @@
+"""sage.openalex module"""
+
+import asyncio
+
+import httpx
+from aiolimiter import AsyncLimiter
+from tenacity import retry, stop_after_attempt, wait_exponential
+
+EMAIL = "email@gmail.com"
+
+openalex_limiter = AsyncLimiter(max_rate=10, time_period=1)
+
+
+@retry(stop=stop_after_attempt(5), wait=wait_exponential(multiplier=1, min=2, max=16))
+async def openalex_get(url: str) -> dict:
+    """
+    Get the paper info from the OpenAlex API.
+    :param url: URL to the OpenAlex API
+    :return: Response JSON from the OpenAlex API
+    """
+    async with openalex_limiter:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url, params={"email": EMAIL})
+            return response.json()
+
+
+async def get_paper_citations(doi: str) -> list[str]:
+    """
+    Get the citations of the paper from the OpenAlex API.
+    :param doi: DOI of the paper
+    :return: List of citations of the paper
+    """
+    url = 'https://api.openalex.org/works/' + doi
+    response = await openalex_get(url)
+    return response.get("referenced_works", [])
+
+
+async def get_paper_references(doi: str) -> list[dict]:
+    """
+    Get the references of the paper from the OpenAlex API.
+    :param doi: DOI of the paper
+    :return: List of references to the paper
+    """
+    url = 'https://api.openalex.org/works?filter=cites:' + doi
+    response = await openalex_get(url)
+    return response.get("results", [])
+
+
+def calculate_similarity_score(paper1: list[str], paper2: list[str]) -> int:
+    """
+    Calculate the similarity score between two papers.
+    :param paper1: List of citations of the first paper
+    :param paper2: List of citations of the second paper
+    :return: Similarity score between the two papers
+    """
+    return len(set(paper1) & set(paper2))
+
+
+async def find_similar_papers(doi: str):
+    """
+    Find similar papers based on the references they share.
+    """
+    citations = await get_paper_citations(doi)
+
+    async def fetch_and_score(citation):
+        """
+        Fetch the references of a paper and calculate the similarity score.
+        :param citation:
+        :return: Tuple of paper ID and similarity score
+        """
+        papers_to_compare = await get_paper_references(citation)
+
+        result = []
+
+        # FIXME: is this the correct way to do this?
+        for item in papers_to_compare:
+            references = item['referenced_works']
+            similarity_score = calculate_similarity_score(citations, references)
+
+            result.append((citation, similarity_score))
+
+        return result
+
+    # Create a list of tasks to run fetch_and_score for each citation concurrently
+    tasks = [fetch_and_score(citation) for citation in citations]
+
+    # Run all tasks concurrently and wait for them to complete
+    results = await asyncio.gather(*tasks)
+
+    # Flatten the list of results
+    # TODO: update this after the above FIXME is fixed
+    results = [result for sublist in results for result in sublist]
+
+    return results
---
+++ tests/test_openalex.py
@@ -0,0 +1,13 @@
+"""tests.test_openalex.py"""
+
+import pytest
+
+from sage.openalex import find_similar_papers
+
+
+@pytest.mark.asyncio
+async def test_find_similar_papers():
+    """Test find_similar_papers()"""
+    doi = "W2741809807"
+    papers = await find_similar_papers(doi)
+    assert len(papers) == 773
