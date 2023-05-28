d511d5a0040dd5317c96a0da74a3bd169743d0a4
Punit Arani <punitsai36@gmail.com>
2023-05-28 01:29:30 -0700

    Add main()

--- app.py
+++ app.py
@@ -13,14 +13,15 @@ from sage.openalex import find_similar_papers
 from sage.summarize import summarize_text_abstractive
 from sage.unpaywall import get_paper_info, download_paper
 
-if __name__ == "__main__":
-    st.set_page_config(page_title="sage", page_icon="🎓", layout="wide")
+
+async def main():
+    """main function"""
     st.title("Sage")
 
     doi_input = st.text_input("DOI", "10.1038/s41586-021-03491-6")
 
     with st.spinner("Fetching paper info..."):
-        paper_info = asyncio.run(get_paper_info(doi_input))
+        paper_info = await get_paper_info(doi_input)
 
     if not paper_info:
         st.error("Invalid DOI")
@@ -33,14 +34,14 @@ if __name__ == "__main__":
         st.json(paper_info)
 
     with st.spinner("Finding similar papers..."):
-        similar_papers = asyncio.run(find_similar_papers(doi_input))
+        similar_papers = await find_similar_papers(doi_input)
 
     with st.expander("Similar papers"):
         for paper in similar_papers:
             st.write(f"{paper[0]} - {paper[1]}")
 
     with st.spinner("Downloading paper..."):
-        download_path = asyncio.run(download_paper(doi_input))
+        download_path = await download_paper(doi_input)
 
     with open(download_path, "rb") as f:
         download_data = f.read()
@@ -48,7 +49,13 @@ if __name__ == "__main__":
 
     with st.spinner("Summarizing paper..."):
         text = load_pdf_document(download_path)
-        summary = asyncio.run(summarize_text_abstractive(text))
+        summary = await summarize_text_abstractive(text)
 
     st.header("Summary")
     st.write(summary)
+
+
+if __name__ == "__main__":
+    st.set_page_config(page_title="sage", page_icon="🎓", layout="wide")
+
+    asyncio.run(main())
