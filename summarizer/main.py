f1a24a21f66fffb32596eeedd05b645c9c293305
Nishita Sikka <nsikka@linkedin.com>
2023-05-27 23:53:18 -0700

    fix

--- summarizer/main.py
+++ summarizer/main.py
@@ -1,3 +1,4 @@
+import time
 from langchain.chains.summarize import load_summarize_chain
 from langchain.document_loaders import PyPDFLoader
 from langchain import OpenAI, PromptTemplate
@@ -14,8 +15,8 @@ class Agent:
 
     def handle_user_input(self, user_input):
         action = self.task_executor.execute_action(user_input)
-        response = self.task_executor.generate_response(action)
-        return response
+        response, response_time = self.task_executor.generate_response(action)
+        return response, response_time
 
 class TaskExecutor:
     def __init__(self):
@@ -23,7 +24,7 @@ class TaskExecutor:
         # openai.api_key = ''  # Replace with your OpenAI API key
         pass
     
-    def pdfs (self, pdfs_folder):
+    def pdfs(self, pdfs_folder):
         return glob.glob(pdfs_folder + "/*.pdf")
 
     def summarize_pdfs_from_folder(self, pdfs_folder):
@@ -42,27 +43,34 @@ class TaskExecutor:
     
     def execute_action(self, user_input):
         # Check user input and determine the appropriate action
-        if user_input.startswith("Summarize the following document:"):
-            pdfs_folder = user_input.replace("Summarize the following document:", "").strip()
+        if user_input.startswith("Summarize the following documents:"):
+            pdfs_folder = user_input.replace("Summarize the following documents:", "").strip()
             return {'type': 'summarize_document', 'document': pdfs_folder}
         else:
             # Handle other actions as needed
             return {}
         
     def generate_response(self, action):
-        # Check the action type
-        if action['type'] == 'summarize_document':
-            pdfs_folder = action['document']
-            summary = self.summarize_pdfs_from_folder(pdfs_folder)
-            return summary
-        else:
-            # Handle other actions as needed
-            return "Unknown action"
+        # Check if the 'type' key exists in the action dictionary
+        if 'type' in action:
+            # Check the action type
+            if action['type'] == 'summarize_document':
+                pdfs_folder = action['document']
+                start_time = time.time()
+                summary = self.summarize_pdfs_from_folder(pdfs_folder)
+                end_time = time.time()
+                response_time = end_time - start_time
+                return summary, response_time
+        
+        # Handle the case where the action type is not recognized
+        return "Unknown action", 0
 
 
 # Example usage
 agent = Agent()
 pdf_folder = "./pdfs"  # Replace "./pdfs" with the desired PDF folder path
 user_input = f"Summarize the following documents: {pdf_folder}"
-response = agent.handle_user_input(user_input)
+response, response_time = agent.handle_user_input(user_input)
 print(response)
+
+print(f"Response Time: {response_time} seconds")
